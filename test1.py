1da5041f7f6d162110ed022d809d059586cb94f7
EJT21 <93630001+EJT21@users.noreply.github.com>
2023-12-13 21:22:03 -0500

    Test1

---
+++ test1.py
@@ -0,0 +1,11 @@
+# Using the walrus operator, introduced in Python 3.8
+
+def check_length(input_list):
+    if (n := len(input_list)) > 5:
+        return f"List is long (length {n})"
+    else:
+        return f"List is short (length {n})"
+
+# Example usage
+result = check_length([1, 2, 3, 4, 5, 6])
+print(result)
